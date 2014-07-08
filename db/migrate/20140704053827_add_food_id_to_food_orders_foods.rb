791a4ef36d630933b216abcf9a70b2aec6cb71f6
syntax-3rror <naty.dany.98@gmail.com>
2014-07-08 16:10:01 -0400

    admin

--- db/migrate/20140704053827_add_food_id_to_food_orders_foods.rb
+++
@@ -1,5 +0,0 @@
-class AddFoodIdToFoodOrdersFoods < ActiveRecord::Migration
-  def change
-    add_column :food_orders_foods, :food_id, :integer
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.new #user_id = 1
+
+admin.role = 'admin'
+admin.email = 'admin@hotmail.com'
+admin.password = 'admin123'
+admin.password_confirmation = 'admin123'
+admin.save!
+
+#usuarios
+usuario1 = User.new #user_id = 2
+usuario1.role = 'waiter'
+usuario1.email = 'usuario1@hotmail.com'
+usuario1.password = '12345678'
+usuario1.password_confirmation = '12345678'
+usuario1.save!
+
+usuario2 = User.new #user_id = 3
+usuario2.role = 'chef'
+usuario2.email = 'usuario2@hotmail.com'
+usuario2.password = '12345678'
+usuario2.password_confirmation = '12345678'
+usuario2.save!
+
+usuario3 = User.new #user_id = 4
+usuario3.role = 'helper'
+usuario3.email = 'usuario3@hotmail.com'
+usuario3.password = '12345678'
+usuario3.password_confirmation = '12345678'
+usuario3.save!
+usuario4 = User.new #user_id = 5
+usuario4.role = 'bartender'
+usuario4.email = 'usuario4@hotmail.com'
+usuario4.password = '12345678'
+usuario4.password_confirmation = '12345678'
+usuario4.save!
